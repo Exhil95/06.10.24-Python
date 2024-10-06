22b6edfc32687b0c9e4efe7196d4d0b6733a958d
Exhil95 <d.kuczka@gmail.com>
2024-10-06 14:21:11 +0200

    "Added a print statement with the text 'wazelina' at the end of the file."

--- operacje_na plikach.py
+++ operacje_na plikach.py
@@ -13,3 +13,5 @@ print(kopia)
 nazwa_nowego_pliku = bezwzglednaSciezka.replace('myInput', 'myOutput')
 with open(nazwa_nowego_pliku, 'x') as f_output:
     f_output.writelines(kopia)
+
+print("wazelina")
