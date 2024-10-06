32f508e4102764ad853ee3269a8a29dfe6e76192
Exhil95 <d.kuczka@gmail.com>
2024-10-06 11:11:32 +0200

    sortuj

---
+++ sort.py
@@ -0,0 +1,12 @@
+lista2 =  ["Grzegorz", "Hieronim", "Inga", "Jolanta", "Karol", "Leszek", 1, 2, 4, 5, "a", "b", "c"]
+
+litery = [x for x in lista2 if isinstance(x, str)]
+cyfry = [x for x in lista2 if isinstance(x, int)]
+
+
+sortuj_litery = sorted(litery)
+sortuj_cyfry = sorted(cyfry)
+
+posortowana_lista = sortuj_litery + sortuj_cyfry
+
+print(posortowana_lista)
