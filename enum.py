c3bc663142535bf7e60ba11d282e54771298adba
Exhil95 <d.kuczka@gmail.com>
2024-10-06 13:37:06 +0200

    enum_1

--- enum.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class  RodzajSilnika(Enum):
-    benzynowy = 1
-    diesel = 2
-    elektryczny = 3
-
-silnik = input("Podaj rodzaj swojego silnika: ")
---
+++ enum_1.py
@@ -0,0 +1,13 @@
+from enum import Enum 
+
+class  RodzajSilnika(Enum):
+    Spalinowy = 1
+    Elektryczny = 2
+    Gazowy = 3
+    
+silnik = input("Podaj rodzaj swojego silnika: ") 
+
+if RodzajSilnika[silnik.capitalize()] == RodzajSilnika.Spalinowy:
+    print("Twój silnik jest nieekologiczny")
+else:
+    print("Twój silnik jest ekologiczny") 
