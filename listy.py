d231a9fb701d5e9c06cbf47f8e47e29f4daa0e90
Exhil95 <d.kuczka@gmail.com>
2024-10-06 11:44:43 +0200

    poprawiona lista

--- listy.py
+++ listy.py
@@ -1,7 +1,6 @@
 lista1 = ['Adam', 'Bogdan', 'Celina', 'Daniel', 'Eliza', 'Florian']
 lista2 =  ['Grzegorz', 'Hieronim', 'Inga', 'Jolanta', 'Karol', 'Leszek', 1, 2, 4, 5]
 
-
 for i in lista1:
     print(i)
     
