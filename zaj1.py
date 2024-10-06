3ff5977f9b4ee7fb1fc3ec8b5431c2434ad00bd4
Exhil95 <d.kuczka@gmail.com>
2024-10-06 09:47:55 +0200

    BMI z interpratacją

--- zaj1.py
+++ zaj1.py
@@ -5,3 +5,12 @@ wzrost = float(input("podaj wzrost w Metrach "))
 bmi = masa/wzrost**2
 
 print("Twoje BMI to:", bmi)
+
+if  bmi < 18.5:
+    print("niedowaga")
+elif 18.5 >=  bmi < 25:
+    print("dobra waga")
+elif  25 >= bmi < 29:
+    print("Nadwaga")
+else :
+    print("Otyłość")
