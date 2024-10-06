6470f2472ce6c9ee33fa2b44fe04580e19f9e7f0
Exhil95 <d.kuczka@gmail.com>
2024-10-06 10:47:54 +0200

    zmieniacz

---
+++ zmiana.py
@@ -0,0 +1,16 @@
+inwokacja = 'Litwo, Ojczyzno moja! ty jesteś jak zdrowie;Ile cię trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś piękność twą w całej ozdobieWidzę i opisuję, bo tęsknię po tobie.'
+
+x = input("Podaj literę do zmienienia lub usunięcia ")
+y = x.lower()
+z = x.capitalize()
+
+a = str(input("na  jaką literę chcesz zmienić/by usunąć wpisz 0: "))
+
+if a == '0':
+    inwokacja = inwokacja.strip(y)
+    inwokacja = inwokacja.strip(z)
+else: 
+    inwokacja = inwokacja.replace(y, a)
+    inwokacja  = inwokacja.replace(z, a)
+    
+print(inwokacja)
